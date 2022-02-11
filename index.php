f59772e6d893e607aad16616bdf236e573a88938
kryptonxarmy <ismynamedanu@gmail.com>
2022-02-11 22:15:53 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,12 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius aperiam, illum quaerat cum numquam eaque? Ex labore voluptas provident, ipsum eveniet id ipsa dignissimos. Vero sint explicabo minus voluptates illum!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+        <li>empat</li>
+        <li>lima</li>
+    </ul>
 </body>
 </html>
